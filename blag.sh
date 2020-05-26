6d8b265ba13e6433d036bbfd4c8a4b2d4d179a33
Caleb Xavier Berger <caleb.x.berger@gmail.com>
2020-05-25 23:01:31 -0400

    Add tag pages

--- blag.sh
+++ blag.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-blagger --out-dir ../c-x-berger.github.io --ignored-files README.md LICENSE.txt blag.sh .gitignore
+blagger --tag-template-html tag_template.html --out-dir ../c-x-berger.github.io --ignored-files README.md LICENSE.txt blag.sh .gitignore
