db7e3678566e52319db19763ee46576bcd0c23dc
Caleb Xavier Berger <caleb.x.berger@gmail.com>
2020-06-01 03:03:06 -0400

    Update to new blagger CLI

--- blag.sh
+++ blag.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-blagger --tag-template-html tag_template.html --out-dir ../c-x-berger.github.io --ignored-files README.md LICENSE.txt blag.sh .gitignore
+blagger --tag-html tag_template.html --out-dir ../c-x-berger.github.io --ignored-files README.md LICENSE.txt blag.sh .gitignore
